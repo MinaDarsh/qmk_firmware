ec5ff870f8f9d6e0a31c5bfd3e3efdebc4074aff
MinaDarsh <nickboots42@gmail.com>
2020-09-01 15:42:54 +0200

    Start development of simpler Function layer.

--- keyboards/ai03/equinox/keymaps/minadarsh/keymap.c
+++ keyboards/ai03/equinox/keymaps/minadarsh/keymap.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include QMK_KEYBOARD_H
+#include QMK_KEYBOARD_H;
 
 enum equinox_layers {
   _COLEMAK,
@@ -34,24 +34,50 @@ enum equinox_keycodes {
 enum {
   DSL = 0,
   CMN,
-  CQT
+  CQT,
+  EF1,
+  1F2,
+  2F3,
+  3F4,
+  4F5,
+  5F6,
+  6F7,
+  7F8,
+  8F9,
+  910,
+  011,
+  E12,
+  B12
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   [DSL] = ACTION_TAP_DANCE_DOUBLE(KC_DOT, KC_SLSH),
   [CMN] = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_MINS),
-  [CQT] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_SCLN)
+  [CQT] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_SCLN),
+  [EF1] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_F1),
+  [1F2] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_F2),
+  [2F3] = ACTION_TAP_DANCE_DOUBLE(KC_2, KC_F3),
+  [3F4] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_F4),
+  [4F5] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_F5),
+  [5F6] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_F6),
+  [6F7] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_F7),
+  [7F8] = ACTION_TAP_DANCE_DOUBLE(KC_7, KC_F8),
+  [8F9] = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_F9),
+  [910] = ACTION_TAP_DANCE_DOUBLE(KC_9, KC_F10),
+  [011] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_F11),
+  [E12] = ACTION_TAP_DANCE_DOUBLE(KC_ENT, KC_F12),
+  [B12] = ACTION_TAP_DANCE_DOUBLE(KC_BSPC, KC_F12)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Colemak-DH
  * ┌────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬──────┐
- * │  TAB   │  Q  │  W  │  F  │  P  │  B  │  J  │  L  │  U  │  Y  │  ;  │ BSPC │
+ * │  TAB   │  Q  │  W  │  F  │  P  │  B  │  J  │  L  │  U  │  Y  │ '/; │ BSPC │
  * ├────────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┐-----│
  * │Backspace│  A  │  R  │  S  │  T  │  G  │  K  │  N  │  E  │  I  │  O  ┋ENTER│
  * ├──────┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴─────┤
- * │LSHIFT┋SYMBL│  Z  │  X  │  C  │  D  │  V  │  M  │  H  │  ,  │  .  ┋ RSHIFT │
+ * │LSHIFT┋SYMBL│  Z  │  X  │  C  │  D  │  V  │  M  │  H  │ ,/- │ .// ┋ RSHIFT │
  * ├──────┼─────┼─────┴┬────┴─────┴───┬─┴───┬─┴─────┴─────┴┬────┴─┬───┴─┬──────┤
  * │L_CTRL│     │ LALT │     SPC      ┋ SPC ┋      SPC     │SYMBOL│     │R_GUI │
  * └──────┘-----└──────┴──────────────┴─────┴──────────────┴──────┘-----└──────┘
@@ -67,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ┌────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬──────┐
  * │        │     │     │  E  │  R  │  T  │  Y  │  U  │  I  │  O  │  P  │      │
  * ├────────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┐-----│
- * │         │     │  S  │  D  │  F  │     │  H  │  J  │  K  │  L  │  ;  ┋     │
+ * │         │     │  S  │  D  │  F  │     │  H  │  J  │  K  │  L  │ '/; ┋     │
  * ├──────┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴─────┤
  * │      ┋     │     │     │     │  V  │  B  │  N  │  M  │     │     ┋        │
  * ├──────┼─────┼─────┴┬────┴─────┴───┬─┴───┬─┴─────┴─────┴┬────┴─┬───┴─┬──────┤
@@ -101,19 +127,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /*
  * ┌────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬──────┐
- * │  ESC   │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │      │
+ * │ ESC/F1 │1/F2 │2/F3 │3/F4 │4/F5 │5/F6 │6/F7 │7/F8 │8/F9 │9/F10│0/F11│BS/F12│
  * ├────────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┐-----│
- * │  GRAVE  │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │  \  │  =  │  [  │  ]  ┋     │
+ * │  GRAVE  │     │     │     │     │     │     │  \  │  =  │  [  │  ]  ┋E/F12│
  * ├──────┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴─────┤
- * │      ┋ADJST│ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ Del │Pg Up│Pg Dn┋        │
+ * │      ┋ADJST│     │     │     │     │     │     │ Del │Pg Up│Pg Dn┋        │
  * ├──────┼─────┼─────┴┬────┴─────┴───┬─┴───┬─┴─────┴─────┴┬────┴─┬───┴─┬──────┤
  * │      │     │      │              ┋     ┋              │ADJUST│     │      │
  * └──────┘-----└──────┴──────────────┴─────┴──────────────┴──────┘-----└──────┘
  */
   [_SYMBOL] = LAYOUT_all( /* Symbol Keys */
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_BSLS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-    _______, ADJUST,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_PGUP, KC_PGDN, _______,
+    TD(EF1), TD(1F2), TD(2F3), TD(3F4), TD(4F5), TD(5F6), TD(6F7), TD(7F8), TD(8F9), TD(910), TD(011), TD(B12),
+    KC_GRV,  _______, _______, _______, _______, _______, _______, KC_BSLS, KC_EQL,  KC_LBRC, KC_RBRC, TD(E12),
+    _______, ADJUST,  _______, _______, _______, _______, _______, _______, KC_DEL,  KC_PGUP, KC_PGDN, _______,
     _______, _______, _______,          _______,          _______,     _______,      ADJUST,  _______, _______
   ),
 
