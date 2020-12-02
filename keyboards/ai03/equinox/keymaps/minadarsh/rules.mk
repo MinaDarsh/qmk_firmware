c09e933ea18272fcd889847a700482f71079dadf
MinaDarsh <nickboots42@gmail.com>
2020-12-02 21:42:47 +0100

    Removed double entry from make file, whoops.

--- keyboards/ai03/equinox/keymaps/minadarsh/rules.mk
+++ keyboards/ai03/equinox/keymaps/minadarsh/rules.mk
@@ -1,5 +1,4 @@
 MOUSEKEY_ENABLE = no			# Mouse keys
 BACKLIGHT_ENABLE = yes		# Enable keyboard backlight functionality
 COMMAND_ENABLE = no				# Commands for debug and configuration
-COMMAND_ENABLE = no				# Commands for debug and configuration
 TAP_DANCE_ENABLE = yes
