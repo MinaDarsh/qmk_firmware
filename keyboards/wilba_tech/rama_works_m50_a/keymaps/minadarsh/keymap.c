1e40dead6865d9762ecd2ad0354aacf0eaa97cd3
MinaDarsh <nickboots42@gmail.com>
2021-07-21 23:55:28 +0200

    Adding M50-A keymap, not compiling yet.

--- keyboards/wilba_tech/rama_works_m50_a/keymaps/minadarsh/keymap.c
+++ keyboards/wilba_tech/rama_works_m50_a/keymaps/minadarsh/keymap.c
@@ -15,81 +15,222 @@
  */
 #include QMK_KEYBOARD_H
 
+enum m50a_layers {
+  _COLEMAK,
+  _QWERTY,
+  _GAMING,
+  _RAISE,
+  _LOWER,
+  _ADJUST
+};
+
+enum m50a_keycodes {
+  COLEMAK = SAFE_RANGE,
+  QWERTY,
+  GAMING
+};
+
+enum {
+  CMN = 0,
+  CQT,
+  GES,
+  TES
+};
+
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+
+void tabesc_finished(qk_tap_dance_state_t *state, void *user_data);
+void tabesc_reset(qk_tap_dance_state_t *state, void *user_data);
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
- * ,------------------------------------------------------------------------------------------.
- * |      | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
- * `------------------------------------------------------------------------------------------'
+/* Colemak-DHk
+ * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+ * │Home │ Tab │  Q  │  W  │  F  │  P  │  B  │  J  │  L  │  U  │  Y  │'(;) │ Del │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │Pg Up│ Esc │  A  │  R  │  S  │  T  │  G  │  K  │  N  │  E  │  I  │  O  │Enter│
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │Pg Dn│LShft│  Z  │  X  │  C  │  D  │  V  │  M  │  H  │,(-) │  .  │  /  │RShft│
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │ End │LCtl │ GUI │LAlt │App  │Lower│Space┋Space│Raise│Left │Down │ Up  │Right│
+ * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
  */
-[0] = LAYOUT(
-    _______, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    _______, KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    _______, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    _______, _______, KC_LCTL, KC_LALT, KC_LGUI, FN_MO13, KC_SPC,  KC_SPC,  FN_MO23, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+[_COLEMAK] = LAYOUT(
+    KC_HOME, TD(TES), KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    TD(CQT), KC_DEL,
+    KC_PGUP, KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_K,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
+    KC_PGDN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_M,    KC_H,    TD(CMN), KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_END,  KC_LCTL, KC_LGUI, KC_LALT, KC_APP,  LOWER,       KC_SPC,       RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
+/* Qwerty
+ * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+ * │     │     │     │     │  E  │  R  │  T  │  Y  │  U  │  I  │  O  │  P  │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │  S  │  D  │  F  │     │  H  │  J  │  K  │  L  │ '(;)│     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │     │     │  V  │  B  │  N  │  M  │     │     │     │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │     │     │     │     ┋     │     │     │     │     │     │
+ * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+ */
+[_QWERTY] = LAYOUT(
+    _______, _______, _______, _______, KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
+    _______, _______, _______, KC_S,    KC_D,    KC_F,    _______, KC_H,    KC_J,    KC_K,    KC_L,    TD(CQT), _______,
+    _______, _______, _______, _______, _______, KC_V,    KC_B,    KC_N,    KC_M,    _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,      _______,     _______, _______, _______, _______, _______
+),
+
+/* Qwerty (Gaming) capslock in original pos, normal Tap-Dance for tab-escape so tab can also be held.
+ * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+ * │     │     │     │     │  E  │  R  │  T  │  Y  │  U  │  I  │  O  │  P  │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │  S  │  D  │  F  │     │  H  │  J  │  K  │  L  │ '(;)│     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │     │     │  V  │  B  │  N  │  M  │     │     │     │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │     │     │     │     ┋     │     │     │     │     │     │
+ * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+ */
+[_GAMING] = LAYOUT(
+    _______, TD(GES), _______, _______, KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
+    _______, _______, _______, KC_S,    KC_D,    KC_F,    _______, KC_H,    KC_J,    KC_K,    KC_L,    TD(CQT), _______,
+    _______, _______, _______, _______, _______, KC_V,    KC_B,    KC_N,    KC_M,    _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,      _______,     _______, _______, _______, _______, _______
+),
 
 /* Lower
- * ,------------------------------------------------------------------------------------------.
- * |      |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `------------------------------------------------------------------------------------------'
+ * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+ * │     │  `  │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │ Del │ N/A │ N/A │ N/A │ N/A │PScrn│Pause│  \  │  =  │  [  │  ]  │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │Prev │Vol -│Vol +│Next │Play │ScLck│Home │ End │Pg Up│Pg Dn│     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │     │     │     │     ┋     │     │     │     │     │     │
+ * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
  */
-[1] = LAYOUT(
-    _______, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    _______, KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+[_LOWER] = LAYOUT(
+    _______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    _______, KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_PAUS, KC_BSLS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+    _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_SLCK, KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______,
+    _______, _______, _______, _______, _______, _______,      _______,     _______, _______, _______, _______, _______
 ),
 
 /* Raise
- * ,------------------------------------------------------------------------------------------.
- * |      |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `------------------------------------------------------------------------------------------'
+ * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+ * │     │  ~  │  !  │  @  │  #  │  $  │  %  │  ^  │  &  │  *  │  (  │  )  │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │  |  │  +  │  {  │  }  │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ N/A │ N/A │ N/A │ N/A │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │     │     │     │     ┋     │     │     │     │     │     │
+ * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
  */
-[2] = LAYOUT(
-    _______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    _______, KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+[_RAISE] = LAYOUT(
+  _______, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+  _______, KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_PIPE, KC_PLUS, KC_LCBR, KC_RCBR, _______,
+  _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+  _______, _______, _______, _______, _______, _______,      _______,     _______, _______, _______, _______, _______
 ),
 
-/* Adjust (Lower + Raise)
- * ,------------------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |             |      |      |      |      |      |
- * `------------------------------------------------------------------------------------------'
+/* Adjust (Lower + Raise) Blank keys are disabled here.
+ * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+ * │     │Reset│Qwrty│     │     │     │     │     │     │     │     │     │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │AGSwp│     │     │     │Game │     │TKNRO│     │     │     │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │     │Colmk│     │     │     │     │     │     │     │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │     │     │     │     ┋     │     │     │     │     │     │
+ * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
  */
-/*
-[3] = LAYOUT(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+[_ADJUST] = LAYOUT(
+    XXXXXXX, RESET,   QWERTY,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, AG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, GAMING,  XXXXXXX, NK_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, COLEMAK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 )
-*/
 
 };
+
+void tabesc_per_tap(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count > 1) {
+    register_code(KC_TAB);
+    if (!state->pressed) {
+      unregister_code(KC_TAB);
+    }
+  }
+}
+
+void tabesc_finished(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    if (state->pressed) {
+      register_code(KC_ESC);
+    } else {
+      register_code(KC_TAB);
+    }
+  }
+}
+
+void tabesc_reset(qk_tap_dance_state_t *state, void *user_data) {
+  unregister_code(KC_ESC);
+  unregister_code(KC_TAB);
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  /*
+  There are two kinds of Tap-Dance Tab/Escapes here, one is for use in text,
+  and one is for use in games. TES has the ability to use Tabs instantly without
+  having to wait for TAPPING_TERM to pass and can be spammed, downside is that
+  Tab can't be held properly in some games, and we don't need to spam it anyway
+  as far as I know, so, just a regular DOUBLE Tap-Dance for the Gaming layer.
+  */
+  [CMN] = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_MINS),
+  [CQT] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_SCLN),
+  [GES] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, KC_ESC),
+  [TES] = ACTION_TAP_DANCE_FN_ADVANCED(tabesc_per_tap, tabesc_finished, tabesc_reset)
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case COLEMAK:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_COLEMAK);
+      }
+      return false;
+      break;
+    case QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case GAMING:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_GAMING);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
+// Template for easy copying.
+
+/*
+ * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+ * │     │     │     │     │     │     │     │     │     │     │     │     │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │     │     │     │     │     │     │     │     │     │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │     │     │     │     │     │     │     │     │     │     │
+ * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │     │     │     │     │     │     │     ┋     │     │     │     │     │     │
+ * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+ */
