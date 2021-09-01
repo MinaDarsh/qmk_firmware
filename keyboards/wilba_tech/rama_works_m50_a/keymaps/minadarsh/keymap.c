32739a7c252e234640e1d840091bbebdc917b4c8
MinaDarsh <nickboots42@gmail.com>
2021-09-01 17:18:41 +0200

    Overhaul of the M50-A keymap.

--- keyboards/wilba_tech/rama_works_m50_a/keymaps/minadarsh/keymap.c
+++ keyboards/wilba_tech/rama_works_m50_a/keymaps/minadarsh/keymap.c
@@ -47,20 +47,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Colemak-DHk
  * ┌─────┐  ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
- * │Home │  │Tb/Es│  Q  │  W  │  F  │  P  │  B  │  J  │  L  │  U  │  Y  │'(;) │ Del │
+ * │  1  │  │Tb/Es│  Q  │  W  │  F  │  P  │  B  │  J  │  L  │  U  │  Y  │'(;) │ Del │
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
- * │Pg Up│  │BkSpc│  A  │  R  │  S  │  T  │  G  │  K  │  N  │  E  │  I  │  O  │Enter│
+ * │  2  │  │BkSpc│  A  │  R  │  S  │  T  │  G  │  K  │  N  │  E  │  I  │  O  │Enter│
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
- * │Pg Dn│  │LShft│  Z  │  X  │  C  │  D  │  V  │  M  │  H  │,(-) │  .  │  /  │RShft│
+ * │  3  │  │LShft│  Z  │  X  │  C  │  D  │  V  │  M  │  H  │,(-) │  .  │  /  │RShft│
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
- * │ End │  │LCtl │ App │ GUI │LAlt │Lower│Space┋Space│Raise│Left │Down │ Up  │Right│
+ * │  4  │  │LCtl │ App │ GUI │LAlt │Lower│Space┋Space│Raise│Left │Down │ Up  │Right│
  * └─────┘  └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
  */
 [_COLEMAK] = LAYOUT(
-    KC_HOME,   TD(TES), KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    TD(CQT), KC_DEL,
-    KC_PGUP,   KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_K,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
-    KC_PGDN,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_M,    KC_H,    TD(CMN), KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_END,    KC_LCTL, KC_APP,  KC_LGUI, KC_LALT, LOWER,       KC_SPC,       RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_1,   TD(TES), KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    TD(CQT), KC_DEL,
+    KC_2,   KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_K,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
+    KC_3,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_M,    KC_H,    TD(CMN), KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_4,    KC_LCTL, KC_APP,  KC_LGUI, KC_LALT, LOWER,       KC_SPC,       RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Qwerty
@@ -83,20 +83,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty (Gaming) capslock in original pos, normal Tap-Dance for tab-escape so tab can also be held, GUI and App Disabled.
  * ┌─────┐  ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
- * │  1  │  │Tb/Es│     │     │     │     │     │     │     │     │     │     │     │
+ * │     │  │Tb/Es│     │     │     │     │     │     │     │     │     │     │     │
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
- * │  2  │  │CapsL│     │     │     │     │     │     │     │     │     │     │     │
+ * │     │  │CapsL│     │     │     │     │     │     │     │     │     │     │     │
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
- * │  3  │  │     │     │     │     │     │     │     │     │     │     │     │     │
+ * │     │  │     │     │     │     │     │     │     │     │     │     │     │     │
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
- * │  4  │  │     │ N/A │ N/A │     │     │     ┋     │     │     │     │     │     │
+ * │     │  │     │ N/A │ N/A │     │     │     ┋     │     │     │     │     │     │
  * └─────┘  └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
  */
 [_GAMING] = LAYOUT(
-    KC_1,      TD(GES), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_2,      KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_3,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_4,      _______, XXXXXXX, XXXXXXX, _______, _______,      _______,     _______, _______, _______, _______, _______
+    _______,   TD(GES), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,   KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,   _______, XXXXXXX, XXXXXXX, _______, _______,      _______,     _______, _______, _______, _______, _______
 ),
 
 /* Raise
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
  * │     │  │     │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │  \  │  =  │  [  │  ]  │     │
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
- * │     │  │     │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ N/A │ N/A │ N/A │ N/A │     │
+ * │     │  │     │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │Home │ End │Pg Up│Pg Dn│     │
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
  * │     │  │     │     │     │     │     │     ┋     │     │Next │Vol -│Vol +│Play │
  * └─────┘  └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT(
     _______,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
     _______,   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_BSLS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-    _______,   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______,   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______,
     _______,   _______, _______, _______, _______, _______,      _______,     _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
  * │     │  │     │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │  |  │  +  │  {  │  }  │     │
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
- * │     │  │     │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ N/A │ N/A │ N/A │ N/A │     │
+ * │     │  │     │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │Home │ End │Pg Up│Pg Dn│     │
  * ├─────┤  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
  * │     │  │     │     │     │     │     │     ┋     │     │Next │Vol -│Vol +│Play │
  * └─────┘  └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   _______,   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   _______,   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_PIPE, KC_PLUS, KC_LCBR, KC_RCBR, _______,
-  _______,   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+  _______,   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______,
   _______,   _______, _______, _______, _______, _______,      _______,     _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
