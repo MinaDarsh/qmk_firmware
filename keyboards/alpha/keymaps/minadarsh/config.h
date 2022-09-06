51b8f78bfbd78d9160470299a79b2f53f39688b6
minadarsh <nickboots42@gmail.com>
2022-09-06 18:18:25 +0200

    Major layer behavior changes to personal Alpha keymap.

--- keyboards/alpha/keymaps/minadarsh/config.h
+++ keyboards/alpha/keymaps/minadarsh/config.h
@@ -5,13 +5,11 @@
 
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD_PER_KEY
+// #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 175
 #define TAPPING_TERM_PER_KEY
 #define COMBO_TERM 60
-
-// Layer timeout when idle and toggled into a layer, resets to main layer
-#define LAYER_IDLE_TIMEOUT 3000 // Milliseconds
 
 // #define USB_POLLING_INTERVAL_MS 1
 // #define QMK_KEYS_PER_SCAN 12
--- keyboards/alpha/keymaps/minadarsh/keymap.c
+++ keyboards/alpha/keymaps/minadarsh/keymap.c
@@ -2,9 +2,9 @@
 
 enum alpha_layers {
   _COLEMAK,
-  _SYMBOL1,
-  _SYMBOL2,
-  _SYMBOL3,
+  _RAISE,
+  _LOWER,
+  _ADJUST,
 };
 
 // defined names for shorter keycode names in the keymap array
@@ -14,11 +14,12 @@ enum alpha_layers {
 #define MC_ALTD MT(MOD_LALT, KC_D)
 #define MC_SFTA MT(MOD_LSFT, KC_A)
 #define MC_SFTO MT(MOD_RSFT, KC_O)
+#define MC_V_RS LT(_RAISE, KC_V)
+#define MC_M_LW LT(_LOWER, KC_M)
 
 enum {
   CQT = 0,
   CMN,
-  LYR
 };
 
 enum combo_events {
@@ -27,7 +28,6 @@ enum combo_events {
   COM_ENT,
   COM_BSP,
   COM_SLH,
-  COM_LYR,
   COM_TAB,
   COM_ESC,
   COM_GRV,
@@ -38,11 +38,10 @@ enum combo_events {
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
 const uint16_t PROGMEM dot_combo[] = {KC_H, MC_CTLZ, COMBO_END};
-const uint16_t PROGMEM com_combo[] = {KC_M, KC_H, COMBO_END};
+const uint16_t PROGMEM com_combo[] = {MC_M_LW, KC_H, COMBO_END};
 const uint16_t PROGMEM ent_combo[] = {KC_I, MC_SFTO, KC_E, COMBO_END};
 const uint16_t PROGMEM bsp_combo[] = {MC_SFTA, KC_R, KC_S, COMBO_END};
-const uint16_t PROGMEM slh_combo[] = {KC_M, MC_CTLZ, COMBO_END};
-const uint16_t PROGMEM lyr_combo[] = {KC_M, KC_H, MC_CTLZ, COMBO_END};
+const uint16_t PROGMEM slh_combo[] = {MC_M_LW, MC_CTLZ, COMBO_END};
 const uint16_t PROGMEM tab_combo[] = {KC_Q, KC_W, COMBO_END};
 const uint16_t PROGMEM esc_combo[] = {KC_Q, KC_W, KC_F, COMBO_END};
 const uint16_t PROGMEM grv_combo[] = {KC_1, KC_2, COMBO_END};
@@ -55,21 +54,12 @@ combo_t key_combos[] = {
   [COM_ENT] = COMBO(ent_combo, KC_ENT),
   [COM_BSP] = COMBO(bsp_combo, KC_BSPC),
   [COM_SLH] = COMBO(slh_combo, KC_SLSH),
-  [COM_LYR] = COMBO(lyr_combo, TD(LYR)),
   [COM_TAB] = COMBO(tab_combo, KC_TAB),
   [COM_ESC] = COMBO(esc_combo, KC_ESC),
   [COM_GRV] = COMBO(grv_combo, KC_GRV),
   [COM_TIL] = COMBO(til_combo, KC_TILD),
   [COM_RST] = COMBO(rst_combo, RESET)
 };
-
-bool td_was_held = false;
-int old_state = 0;
-bool is_idle = true;
-static uint16_t timer;
-
-void layers_finished(qk_tap_dance_state_t *state, void *user_data);
-void layers_reset(qk_tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Colemak-DHk
@@ -78,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
  * │  A  │  R  │  S  │  T  │  G  │  K  │  N  │  E  │  I  │  O  │
  * └──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴──┬──┴──┬──┘
- *    │X/Ctl│C/Alt│D/GUI│  V  │    Spc    │  M  │  H  │Z/CTL│
+ *    │X/Ctl│C/Alt│D/GUI│V/RSE│    Spc    │M/LWR│  H  │Z/CTL│
  *    └─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┘
  *  main layer
  *
@@ -90,7 +80,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    - M and H form Comma
  *    - H and Z/CTL form Dot
  *    - M and Z/CTL form Forward Slash
- *    - M, H and Z/CTL form the Special Layer Key
  *    - Q, X, Z/CTL and O to enter bootloader mode
  *
  *  keys with a '/' are Mod-Tap keys, key before '/' is on tap, after is on hold
@@ -101,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_COLEMAK] = LAYOUT(
 		KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    TD(CQT),
 		MC_SFTA, KC_R,    KC_S,    KC_T,    KC_G,    KC_K,    KC_N,    KC_E,    KC_I,    MC_SFTO,
-	           MC_CTLX, MC_GUIC, MC_ALTD, KC_V,    KC_SPC,  KC_M,    KC_H,    MC_CTLZ
+	           MC_CTLX, MC_GUIC, MC_ALTD, MC_V_RS, KC_SPC,  MC_M_LW, KC_H,    MC_CTLZ
   ),
 /*
  * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
@@ -109,17 +98,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
  * │Left │ Up  │Down │Right│ Ins │ Del │  \  │  =  │  [  │  ]  │
  * └──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴──┬──┴──┬──┘
- *    │Home │ End │Pg Up│Pg Dn│           │ keep these free!│
+ *    │Home │ End │     │!keep│           │free!│Pg Up│Pg Dn│
  *    └─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┘
  *  numbers and unshifted characters
  *  
  *  one combo on this layer:
  *    - 1 and 2 form Grave
  */
-	[_SYMBOL1] = LAYOUT(
+	[_RAISE] = LAYOUT(
 		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,  KC_DEL,  KC_BSLS, KC_EQL,  KC_LBRC, KC_RBRC,
-             KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______, _______, _______, _______
+             KC_HOME, KC_END,  _______, _______, _______, _______, KC_PGUP, KC_PGDN
   ),
 /*
  * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
@@ -127,17 +116,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
  * │Left │ Up  │Down │Right│ Ins │ Del │  |  │  +  │  {  │  }  │
  * └──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴──┬──┴──┬──┘
- *    │Home │ End │Pg Up│Pg Dn│           │ keep these free!│
+ *    │Home │ End │     │!keep│           │free!│Pg Up│Pg Dn│
  *    └─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┘
  *  shifted characters and other outputs
  *  
  *  one combo on this layer:
  *    - ! and @ form Tilde
  */
-	[_SYMBOL2] = LAYOUT(
+	[_LOWER] = LAYOUT(
 		KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,  KC_DEL,  KC_PIPE, KC_PLUS, KC_LCBR, KC_RCBR,
-             KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______, _______, _______, _______
+             KC_HOME, KC_END, _______, _______, _______, _______,  KC_PGUP, KC_PGDN
   ),
 /*
  * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
@@ -145,84 +134,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
  * │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ N/A │ N/A │ N/A │ N/A │
  * └──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴─────┴──┬──┴──┬──┴──┬──┴──┬──┘
- *    │Prev │Vol -│Vol +│Next │   Play    │ keep these free!│
+ *    │Prev │Next │Play │!keep│           │free!│Vol -│Vol +│
  *    └─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┘
  *  function and media keys
  */
-	[_SYMBOL3] = LAYOUT(
+	[_ADJUST] = LAYOUT(
 		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_PSCR, KC_SLCK, KC_PAUS, XXXXXXX,
     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-             KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, _______, _______, _______
+             KC_MPRV, KC_MNXT, KC_MPLY, _______, _______, _______, KC_VOLD, KC_VOLU
   )
 };
 
-void matrix_scan_user(void) {
-  if (!is_idle && !td_was_held) {
-    if (timer_elapsed(timer) >= (uint16_t) LAYER_IDLE_TIMEOUT * 1000) {
-      is_idle = true;
-      if (!layer_state_is(_COLEMAK)) {
-        layer_move(_COLEMAK);
-        old_state = 0;
-      }
-    }
-  }
-}
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [CQT] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_SCLN),
+  [CMN] = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_MINS)
+};
 
+#ifdef TAPPING_TERM_PER_KEY
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case TD(LYR):
-            return 250;
-        case TD(CQT):
-            return 175;
-        case TD(CMN):
-            return 175;
+        case MC_SFTA:
+        case MC_SFTO:
+            return 125;
         default:
             return TAPPING_TERM;
     }
 }
+#endif
 
-void layers_per_tap(qk_tap_dance_state_t *state, void *user_data) {
-  switch (state->count) {
-    case 1:
-      layer_move(_SYMBOL1);
-      break;
-    case 2:
-      layer_move(_SYMBOL2);
-      break;
-    case 3:
-      layer_move(_SYMBOL3);
-      break;
-    default:
-      layer_move(_COLEMAK);
-      old_state = 0;
-  }
-}
-
-void layers_finished(qk_tap_dance_state_t *state, void *user_data) {
-  if (state->pressed) {
-    td_was_held = true;
-  } else if (state->count == old_state) {
-    layer_move(_COLEMAK);
-    old_state = 0;
-  } else {
-    old_state = state->count;
-  }
-}
-
-void layers_reset(qk_tap_dance_state_t *state, void *user_data) {
-  if (td_was_held == true) {
-    layer_move(_COLEMAK);
-    old_state = 0;
-    td_was_held = false;
-  }
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [CQT] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_SCLN),
-  [CMN] = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_MINS),
-  [LYR] = ACTION_TAP_DANCE_FN_ADVANCED(layers_per_tap, layers_finished, layers_reset)
-};
-
+#ifdef PERMISSIVE_HOLD_PER_KEY
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case MC_SFTA:
@@ -233,12 +173,26 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
       return false;
   }
 }
+#endif
+
+#ifdef HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case MC_SFTA:
+      return true;
+    case MC_SFTO:
+      return true;
+    default:
+      return false;
+    }
+}
+#endif
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  timer = timer_read();
-  if (is_idle) {
-    is_idle = false;
-  }
   if (keycode != KC_SPC) return true;
 
   if ( !((get_mods() & MOD_BIT(KC_LSFT)) == MOD_BIT(KC_LSFT)) && !((get_mods() & MOD_BIT(KC_LCTL)) == MOD_BIT(KC_LCTL)) ) {
