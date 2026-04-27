277850922fab130d88f1d7b9ad02357facd36015
MinaDarsh <nickboots42@gmail.com>
2026-04-27 20:11:04 +0200

    Added personal keymap to Cipquinox.

---
+++ keyboards/cipulot/cipquinox/keymaps/minadarsh/rules.mk
@@ -0,0 +1,6 @@
+MOUSEKEY_ENABLE = no			# Mouse keys
+BACKLIGHT_ENABLE = no			# Enable keyboard backlight functionality
+COMMAND_ENABLE = no				# Commands for debug and configuration
+TAP_DANCE_ENABLE = yes
+BOOTMAGIC_ENABLE = yes   # Virtual DIP switch configuration
+COMBO_ENABLE = no				# Combos for pressing multiple keys to trigger another action
